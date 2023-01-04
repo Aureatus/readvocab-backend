111538d872bad7c3c4fec7bc8082dbc721292735
Alex Knott <alextknott@gmail.com>
2023-01-04 18:26:20 +0000

    Refactor using prisma

--- src/helpers/getCachedResult.ts
+++ src/helpers/getCachedResult.ts
@@ -1,21 +1,21 @@
 import type { PDFDocumentProxy } from "pdfjs-dist";
 import type { DefinitionWord } from "../types.js";
 
-import type { Db } from "mongodb";
+import type { PrismaClient } from "@prisma/client";
 
 const getCachedResult = async (
   docProxy: PDFDocumentProxy,
-  db: Db
+  prisma: PrismaClient
 ): Promise<DefinitionWord[] | null> => {
+  // Potentially create a GO microservice
   const { metadata } = await docProxy.getMetadata();
   const title = metadata.get("dc:title");
   const creator = metadata.get("dc:creator");
 
-  const pdfCollection = db.collection<{ data: DefinitionWord[] }>("pdfs");
-  const response = await pdfCollection.findOne(
-    { title, creator },
-    { projection: { data: 1 } }
-  );
+  const response = await prisma.pdfs.findUnique({
+    where: { creator_title: { creator, title } },
+    select: { data: true },
+  });
 
   const data = response?.data ?? null;
 
